93d2d88a60e5cf2ad6c856071326d6aef89ea6e3
Rutjake <jarnosanterimusic@gmail.com>
2023-10-25 17:57:47 +0300

    Get number of people on list and fetch date.

--- index.js
+++ index.js
@@ -32,8 +32,11 @@ app.get('/', (request, response) => {
     response.send('<h1>Hello!<h1>')
 })
 
+// Information how many people on the list & Fetching Time/Date
 app.get('/info', (request, response) => {
-    response.json(`Phonebook has info for ${persons.length} people`)
+    //Get Server time
+    const dateTime = new Date()
+    response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${dateTime}</p>`)
 })
 
 // Get All Info From JSON Table
@@ -41,6 +44,17 @@ app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
 
+// Get Info By Id And Send Error If Not Found
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+        response.json(person)
+    }else{
+        response.status(404).end()
+    }
+})
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
